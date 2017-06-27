7f7927847cc356a6e833a045cc6bbda00e990892
DeviousD <dskleingeld@gmail.com>
2017-06-27 19:32:09 +0200

    code formatting

--- server/main.cpp
+++ server/main.cpp
@@ -14,76 +14,73 @@ void* arrayOfPointers[4];
 
 int main(int argc, char* argv[])
 {
-	int port = config::HTTPSERVER_PORT;
-	const char* keyPath = "privkey1.pem";
-	const char* certPath = "fullchain1.pem"; 
+  int port = config::HTTPSERVER_PORT;
+  const char* keyPath = "privkey1.pem";
+  const char* certPath = "fullchain1.pem";
 
-	//parse arguments
-	if(argc > 1){
-		port = atoi(argv[0]);
-		if(argc > 2){
-			keyPath = argv[1];
-			if(argc > 3){
-				certPath = argv[2];
-			}
-		}
-	}
+  //parse arguments
+  if(argc > 1){
+    port = atoi(argv[0]);
+    if(argc > 2){
+      keyPath = argv[1];
+      if(argc > 3){
+        certPath = argv[2];
+      }
+    }
+  }
 
-	//load the ssl key and certificate (needed for https)										
+  //load the ssl key and certificate (needed for https)                    
   key_pem = load_file(keyPath);
   cert_pem = load_file(certPath);
 
   key_pem = load_file("privkey1.pem");
   cert_pem = load_file("fullchain1.pem");
 
-  //check if key and cert where read okay
-  if ((key_pem == NULL) || (cert_pem == NULL))
-  {
+  //check if key and cert were read okay
+  if ((key_pem == NULL) || (cert_pem == NULL)) {
     std::cout<<"The key/certificate files could not be read.\n"
     <<"please put \""<<keyPath<<"\" and \""<<keyPath<<"\" in the same dir as this "
     <<"program.\n";
     return 1;
   }
 
-	//open file for writing
-	std::ofstream outfile("data.txt", std::ofstream::out | std::ofstream::app);
-	if(!outfile.is_open()){std::cerr<<"Couldn't open 'output.txt'\n"; return 1;}
-	
-	//start the server
+  //open file for writing
+  std::ofstream outfile("data.txt", std::ofstream::out | std::ofstream::app);
+  if(!outfile.is_open()){std::cerr<<"Couldn't open 'output.txt'\n"; return 1;}
+  
+  //start the server
   server = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY | MHD_USE_SSL,
-														 port, NULL, NULL,
+                             port, NULL, NULL,
                              &answer_to_connection, toVoidArr(&outfile),
                              MHD_OPTION_HTTPS_MEM_KEY, key_pem,
                              MHD_OPTION_HTTPS_MEM_CERT, cert_pem,
-														 MHD_OPTION_END);
+                             MHD_OPTION_END);
   
   //check if the server started alright                           
-  if(NULL == server)
-    {
-  		std::cout<<"server could not start\n";
+  if(NULL == server) {
+      std::cout<<"server could not start\n";
       
       //free memory if the server crashed
       delete key_pem;
       delete cert_pem;
 
       return 1;
-    }
-	else{
-		std::cout<<"started server with paramaters:\n  port="
-		<<port<<", ssl key:"<<keyPath<<", ssl cert:"<<certPath<<"\n"
-		<<"\nPress [Enter] to close the server";
-	
-	} 
+  } else {
+    std::cout<<"started server with paramaters:\n  port="
+    <<port<<", ssl key:"<<keyPath<<", ssl cert:"<<certPath<<"\n"
+    <<"\nPress [Enter] to close the server";
+  
+  } 
 
-	getchar();	//wait for enter to shut down
-	std::cout<<"shutting down server\n";
-	
+  getchar();  //wait for enter to shut down
+  std::cout<<"shutting down server\n";
+  
   //free memory if the server stops
   MHD_stop_daemon(server);
   delete key_pem;
   delete cert_pem;
   
-	std::cout<<"done\n";
+  std::cout<<"done\n";
   return 0;      
 }
 
--- server/mainServer.cpp
+++ server/mainServer.cpp
@@ -20,102 +20,98 @@ inline uint32_t unix_timestamp() {
 }
 
 inline int authorised_connection(struct MHD_Connection* connection){
-	std::cout<<"checking if authorised\n";
-	bool fail;
-	char* pass = NULL;
-	char* user = MHD_basic_auth_get_username_password(connection, &pass);
-	fail = ( (user == NULL) ||
-				 (0 != strcmp (user, config::HTTPSERVER_USER)) ||
-				 (0 != strcmp (pass, config::HTTPSERVER_PASS)) );  
-	if (user != NULL) delete user;
-	if (pass != NULL) delete pass;
+  bool fail = true;
+  char* pass = NULL;
+  char* user = MHD_basic_auth_get_username_password(connection, &pass);
+  fail = ( (user == NULL) ||
+         (0 != strcmp(user, config::HTTPSERVER_USER)) ||
+         (0 != strcmp(pass, config::HTTPSERVER_PASS)) );  
+  if (user != NULL) delete user;
+  if (pass != NULL) delete pass;
 
-	std::cout<<"user: "<<user<<"\n";
-	std::cout<<"pass: "<<pass<<"\n";
-
-	std::cout<<"fail: "<<fail<<"\n";
-	return !fail ; //FIXME TODO SECURITY RISK
-	//return true;
+  return !fail ; //FIXME TODO SECURITY RISK
+  //return true;
 }
 
 
 int answer_to_connection(void* cls,struct MHD_Connection* connection, const char* url,
-		                     const char* method, const char* version, const char* upload_data,
-		                     size_t* upload_data_size, void** con_cls) {
-	std::cout<<"AWNSERING\n";
+                         const char* method, const char* version, const char* upload_data,
+                         size_t* upload_data_size, void** con_cls) {
+  std::cout<<"ANSWERING\n";
   int ret;
-  struct MHD_Response *response;	
-	std::ofstream* outfile;
-	fromVoidArr(cls, outfile);
+  struct MHD_Response *response;  
+  std::ofstream* outfile;
+  fromVoidArr(cls, outfile);
 
-	//if start of connection, remember connection
-	if (NULL == *con_cls) {*con_cls = connection; return MHD_YES;}
-
-
-	//if(strcmp(method, MHD_HTTP_METHOD_GET) == 0){
-
-		//correct password, repond dependig on url
-		if(authorised_connection(connection)){
-			std::cout<<"VERIFIED\n";
-			//create diffrent pages (responses) to diffrent url's
-			if(strcmp(url, "/putData") == 0){
-				response = MHD_create_response_from_buffer(strlen (test_page), 
-				(void *) test_page, MHD_RESPMEM_PERSISTENT);
-				std::lock_guard<std::mutex> lock(outfile_mutex);
-				(*outfile)<<"test\n"; //std::to_string(unix_timestamp())+
-			}
-			else if(strcmp(url, "/test") == 0){
-				response = MHD_create_response_from_buffer(strlen (test_page), 
-				(void *) test_page, MHD_RESPMEM_PERSISTENT);			
-			}
-			else{
-				response = MHD_create_response_from_buffer(strlen (unknown_page), 
-				(void *) unknown_page, MHD_RESPMEM_PERSISTENT);					
-			}
-
-		//prepare respons to be send to server
-		ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
-		}
-
-		else{//incorrect password, present go away page
-			std::cout<<"NOT VERIFIED\n";
-			const char* page = "<html><body>Incorrect password.</body></html>";	
-			response = MHD_create_response_from_buffer(strlen (page), (void*) page, 
-								 MHD_RESPMEM_PERSISTENT);
-			ret = MHD_queue_basic_auth_fail_response(connection, "test", response);	
-		}
-	//}
-	//else{ return MHD_NO;}
-
-
-  MHD_destroy_response (response); //free memory of the respons
+  //if start of connection, remember connection
+  if (NULL == *con_cls) {
+    *con_cls = connection;
+    return MHD_YES;
+  }
+  
+  //correct password, repond dependig on url
+  if (authorised_connection(connection)){
+    std::cout<<"VERIFIED\n";
+    
+    if (strcmp(method, MHD_HTTP_METHOD_GET) == 0){
+      //create diffrent pages (responses) to different url's
+      if (strcmp(url, "/putData") == 0) {
+        response = MHD_create_response_from_buffer(strlen (test_page), 
+        (void *) test_page, MHD_RESPMEM_PERSISTENT);
+        std::lock_guard<std::mutex> lock(outfile_mutex);
+        (*outfile)<<"test\n"; //std::to_string(unix_timestamp())+
+      }
+      else if (strcmp(url, "/test") == 0) {
+        response = MHD_create_response_from_buffer(strlen (test_page), 
+        (void *) test_page, MHD_RESPMEM_PERSISTENT);      
+      }
+      else {
+        response = MHD_create_response_from_buffer(strlen (unknown_page), 
+        (void *) unknown_page, MHD_RESPMEM_PERSISTENT);          
+      }
+    //prepare respons to be send to server
+    ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+    }
+    
+    else if (strcmp(method, MHD_HTTP_METHOD_POST) == 0) {
+      
+    }
+    
+  //incorrect password, present go away page
+  }
+  else {
+    std::cout<<"NOT VERIFIED\n";
+    const char* page = "<html><body>Incorrect password.</body></html>";  
+    response = MHD_create_response_from_buffer(strlen (page), (void*) page, 
+	       MHD_RESPMEM_PERSISTENT);
+    ret = MHD_queue_basic_auth_fail_response(connection, "test", response);  
+  }
+  
+  MHD_destroy_response(response); //free memory of the respons
   return ret;
 }
 
 //used by load_file to find out the file size
-long get_file_size (const char *filename)
-{
+long get_file_size(const char *filename) {
   FILE *fp;
 
   fp = fopen (filename, "rb");
-  if (fp)
-    {
-      long size;
+  if (fp) {
+    long size;
 
-      if ((0 != fseek (fp, 0, SEEK_END)) || (-1 == (size = ftell (fp))))
-        size = 0;
+    if ((0 != fseek (fp, 0, SEEK_END)) || (-1 == (size = ftell (fp))))
+      size = 0;
 
-      fclose (fp);
+    fclose(fp);
 
-      return size;
-    }
+    return size;
+  }
   else
     return 0;
 }
 
 //used to load the key files into memory
-char* load_file (const char *filename)
-{
+char* load_file(const char *filename) {
   FILE *fp;
   char* buffer;
   unsigned long size;
@@ -125,44 +121,42 @@ char* load_file (const char *filename)
     return NULL;
 
   fp = fopen(filename, "rb");
-  if (! fp)
+  if (!fp)
     return NULL;
 
   buffer = (char*)malloc(size + 1);
-  if (! buffer)
-    {
+  if (!buffer) {
       fclose (fp);
       return NULL;
-    }
+  }
   buffer[size] = '\0';
 
-  if (size != fread (buffer, 1, size, fp))
-    {
-      free (buffer);
+  if (size != fread(buffer, 1, size, fp)) {
+      free(buffer);
       buffer = NULL;
-    }
+  }
 
-  fclose (fp);
+  fclose(fp);
   return buffer;
 }
 
 
 void* toVoidArr(std::ofstream* outfile){
-	void* arrayOfPointers[1];
+  void* arrayOfPointers[1];
 
-	//convert arguments
-	arrayOfPointers[0] = (void*)outfile;
+  //convert arguments
+  arrayOfPointers[0] = (void*)outfile;
 
-	return (void*)arrayOfPointers;																		 
+  return (void*)arrayOfPointers;                                     
 }
 
 inline void fromVoidArr(void* cls, std::ofstream*& outfile){
-	void** arrayOfPointers;
-	void* element1;
+  void** arrayOfPointers;
+  void* element1;
 
-	//convert arguments back (hope this optimises well),
-	arrayOfPointers = (void**)cls;
-	element1 = (void*)*(arrayOfPointers+0);
-	outfile = (std::ofstream*)element1;
-	return;																		 
+  //convert arguments back (hope this optimises well),
+  arrayOfPointers = (void**)cls;
+  element1 = (void*)*(arrayOfPointers+0);
+  outfile = (std::ofstream*)element1;
+  return;                                     
 }
